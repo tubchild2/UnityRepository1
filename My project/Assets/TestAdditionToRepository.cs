d63809fb4124a26eb6bc33582623a155d372ad53
tubchild2 <132407253+tubchild2@users.noreply.github.com>
2023-05-08 13:03:15 -0700

    Added Velocity script

--- My project/Assets/TestAdditionToRepository.cs
+++ My project/Assets/TestAdditionToRepository.cs
@@ -7,7 +7,7 @@ public class TestAdditionToRepository : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 99, 0);
     }
 
     // Update is called once per frame
